ea201e1bd04a2b503804fa4fe27df2ed7483bad6
elnathanau1 <elnathanau1@gmail.com>
2020-11-06 22:22:00 -0600

    Search

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 # plex-download-api
 API written to download media from the web for Plex server
 
-### Running server:
+## API Spec
+### Search
+`/search` - Hits search endpoint of api source.
+
+Query params:
+- `query`: String
+
+Example response body:
+```
+{
+  "source": "gogo-stream",
+  "search_endpoint": "https://gogo-stream.com/ajax-search.html",
+  "search_results": [
+    {
+      "name": "Kimetsu no Yaiba",
+      "url": "https://gogo-stream.com/videos/kimetsu-no-yaiba-episode-26"
+    },
+    {
+      "name": "Kimetsu no Yaiba (Dub)",
+      "url": "https://gogo-stream.com/videos/kimetsu-no-yaiba-dub-episode-26"
+    }
+  ]
+}
+```
+
+
+
+## Running server:
 Runs on localhost:9050.
 
 Move service file using
--- app.py
+++ app.py
@@ -1,9 +1,42 @@
-from flask import Flask
+from flask import Flask, request
+from resources.gogo_stream_service import search as gogo_stream_search
+from resources.gogo_stream_service import test as test_search
 app = Flask(__name__)
+
+DEFAULT_SOURCE = 'GOGO-STREAM'
+SUPPORTED_SOURCES = [
+    'GOGO-STREAM',
+    'TEST'
+]
+SEARCH_FUNCTION_MAP = {
+    'GOGO-STREAM' : gogo_stream_search,
+    'TEST' : test_search
+}
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/search")
+def search():
+    # get query
+    query = request.args.get('query')
+    if query is None:
+        query = ''
+    # set api_source from header
+    api_source = get_api_source()
+
+    return SEARCH_FUNCTION_MAP[api_source](query)
+
+def get_api_source():
+    # check for X-API-SOURCE header
+    api_source = DEFAULT_SOURCE
+    if 'X-API-SOURCE' in request.headers:
+        new_api_source = request.headers['X-API-SOURCE'].upper()
+        if new_api_source in SUPPORTED_SOURCES:
+            api_source = new_api_source
+
+    return api_source
 
 if __name__ == '__main__':
     app.run(host="localhost", port=9050, debug=True)
