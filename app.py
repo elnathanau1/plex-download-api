d85f7c33db75beda32c07d70f461f4578183fa31
elnathanau1 <elnathanau1@gmail.com>
2021-10-05 12:50:14 -0500

    add gowatchseries

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from resources.gogo_stream_service import search as gogo_stream_search
 from resources.gogo_stream_service import get_episode_download_link as gogo_stream_get_episode_download_link
 from resources.gogo_stream_service import show as gogo_stream_show
 from resources.gogo_stream_service import get_movie_download_link as gogo_stream_get_movie_download_link
+from resources.gowatchseries_service import get_episode_download_link as gowatchseries_get_episode_download_link
+from resources.gowatchseries_service import show as gowatchseries_show
+from resources.gowatchseries_service import get_movie_download_link as gowatchseries_get_movie_download_link
 from resources.animeflix_service import get_episode_download_link as animeflix_get_episode_download_link
 from resources.animeflix_service import show as animeflix_show
 from resources.animeflix_service import get_movie_download_link as animeflix_get_movie_download_link
@@ -48,13 +51,15 @@ SEARCH_FUNCTION_MAP = {
 SHOW_FUNCTION_MAP = {
     'GOGO-STREAM': gogo_stream_show,
     'ANIMEFLIX': animeflix_show,
-    'BMOVIES': bmovies_show
+    'BMOVIES': bmovies_show,
+    'GOWATCHSERIES': gowatchseries_show
 }
 
 GET_EPISODE_DOWNLOAD_LINK_FUNCTION_MAP = {
     'GOGO-STREAM': gogo_stream_get_episode_download_link,
     'ANIMEFLIX': animeflix_get_episode_download_link,
     'BMOVIES': bmovies_get_episode_download_link,
+    'GOWATCHSERIES': gowatchseries_get_episode_download_link
 }
 
 GET_MOVIE_DOWNLOAD_LINK_FUNCTION_MAP = {
@@ -62,7 +67,8 @@ GET_MOVIE_DOWNLOAD_LINK_FUNCTION_MAP = {
     'DOWNLOAD-ANYMOVIES': download_anymovies_get_movie_download_link,
     'BMOVIES': bmovies_get_movie_download_link,
     'GOGO-STREAM': gogo_stream_get_movie_download_link,
-    'ANIMEFLIX': animeflix_get_movie_download_link
+    'ANIMEFLIX': animeflix_get_movie_download_link,
+    'GOWATCHSERIES': gowatchseries_get_movie_download_link
 }
 
 
---
+++ resources/gowatchseries_service.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from resources.sbplay_scraper import scrape_download_link as sbplay_scrape
+
+MAIN_URL = 'https://gowatchseries.online'
+
+
+def get_movie_download_link(url):
+    return (get_episode_download_link(url), {})
+
+
+def generate_episode_json(entry):
+    url = MAIN_URL + entry.find('a').get('href')
+    ep_num = re.match(r'.+Episode (.+?)(?:\Z| )+', entry.find('a')['title']).group(1)
+    return {
+        'url': MAIN_URL + url,
+        'ep_num': ep_num
+    }
+
+
+def show(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    episode_entries = soup.find_all('li', {'class': 'child_episode'})
+    episodes = list(map(generate_episode_json, episode_entries))
+
+    return_map = {
+        'episodes': episodes
+    }
+
+    return return_map
+
+
+def get_episode_download_link(url):
+    try:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, features='html.parser')
+        embed_link = 'https:' + soup.find('iframe').get('src')
+
+        r = requests.get(embed_link)
+        soup = BeautifulSoup(r.content, features='html.parser')
+        link_servers = soup.find_all('li', {'class': 'linkserver'})
+        try:
+            sbplay_link = next(link for link in link_servers if 'sbplay' in link['data-video'])['data-video'].replace(
+                'embed-', '')
+            return sbplay_scrape(sbplay_link)
+        except StopIteration:
+            print("sbplay not found for {}".format(url))
+            return None
+
+    except Exception as e:
+        print(url)
+        return None
