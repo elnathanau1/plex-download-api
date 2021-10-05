4b6c173e7fe6ef46792b72edb8cebbe577528c5f
elnathanau1 <elnathanau1@gmail.com>
2021-10-05 13:11:25 -0500

    Add extra attempt for normal quality if high does not exist

--- resources/sbplay_scraper.py
+++ resources/sbplay_scraper.py
@@ -3,24 +3,31 @@ from bs4 import BeautifulSoup
 import re
 
 
+# TODO: REFACTOR THIS
 def scrape_download_link(sbplay_embed_link):
     r = requests.get(sbplay_embed_link)
     soup = BeautifulSoup(r.content, features='html.parser')
     download_link_button = soup.find('a', {'href': '#'})
     regex_match = re.match(r'download_video\(\'(\w+)\',\'\w\',\'(.+)\'\)', download_link_button['onclick'])
     id = regex_match.group(1)
+    mode = 'h'
     hash = regex_match.group(2)
-    # TODO: add safety around if mode=h (high) exists. mode=n is another option
-    download_page_link = 'https://sbplay.one/dl?op=download_orig&id={}&mode=h&hash={}'.format(id, hash)
+    download_page_link = 'https://sbplay.one/dl?op=download_orig&id={}&mode={}&hash={}'.format(id, mode, hash)
 
     r = requests.get(download_page_link)
     soup = BeautifulSoup(r.content, features='html.parser')
     span = soup.find('span', {'style': 'background:#f9f9f9;border:1px dotted #bbb;padding:7px;'})
+    if span is None:
+        mode = 'n'
+        download_page_link = 'https://sbplay.one/dl?op=download_orig&id={}&mode={}&hash={}'.format(id, mode, hash)
+        r = requests.get(download_page_link)
+        soup = BeautifulSoup(r.content, features='html.parser')
+        span = soup.find('span', {'style': 'background:#f9f9f9;border:1px dotted #bbb;padding:7px;'})
+
     # occasionally there is security issue, but is just a button press to bypass.
     if span is None:
         hash = soup.find('input', {'name': 'hash'})['value']
-        download_page_link = 'https://sbplay.one/dl?op=download_orig&id={}&mode=h&hash={}'.format(id, hash)
-        print(download_page_link)
+        download_page_link = 'https://sbplay.one/dl?op=download_orig&id={}&mode=h&hash={}'.format(id, mode, hash)
 
         r = requests.get(download_page_link)
         soup = BeautifulSoup(r.content, features='html.parser')
