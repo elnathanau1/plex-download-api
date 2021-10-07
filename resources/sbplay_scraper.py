df6720f64ad9838dc5f07d924ca538ccb662623a
elnathanau1 <elnathanau1@gmail.com>
2021-10-06 21:27:51 -0500

    refactor sbplay settings

--- resources/sbplay_scraper.py
+++ resources/sbplay_scraper.py
@@ -10,28 +10,31 @@ def scrape_download_link(sbplay_embed_link):
     download_link_button = soup.find('a', {'href': '#'})
     regex_match = re.match(r'download_video\(\'(\w+)\',\'\w\',\'(.+)\'\)', download_link_button['onclick'])
     id = regex_match.group(1)
-    mode = 'h'
     hash = regex_match.group(2)
-    download_page_link = 'https://sbplay.one/dl?op=download_orig&id={}&mode={}&hash={}'.format(id, mode, hash)
 
-    r = requests.get(download_page_link)
-    soup = BeautifulSoup(r.content, features='html.parser')
-    span = soup.find('span', {'style': 'background:#f9f9f9;border:1px dotted #bbb;padding:7px;'})
-    if span is None:
-        mode = 'n'
-        download_page_link = 'https://sbplay.one/dl?op=download_orig&id={}&mode={}&hash={}'.format(id, mode, hash)
+    attempt_list = [
+        (id, 'h', hash),
+        (id, 'o', hash),
+        (id, 'n', hash),
+        (id, 'l', hash)
+    ]
+
+    for attempt in attempt_list:
+        download_page_link = 'https://sbplay.one/dl?op=download_orig&id={}&mode={}&hash={}'.format(*attempt)
         r = requests.get(download_page_link)
         soup = BeautifulSoup(r.content, features='html.parser')
         span = soup.find('span', {'style': 'background:#f9f9f9;border:1px dotted #bbb;padding:7px;'})
 
-    # occasionally there is security issue, but is just a button press to bypass.
-    if span is None:
-        hash = soup.find('input', {'name': 'hash'})['value']
-        download_page_link = 'https://sbplay.one/dl?op=download_orig&id={}&mode=h&hash={}'.format(id, mode, hash)
+        # occasionally there is security issue, but is just a button press to bypass.
+        if span is None:
+            hash = soup.find('input', {'name': 'hash'})['value']
+            download_page_link = 'https://sbplay.one/dl?op=download_orig&id={}&mode=h&hash={}'.format(id, mode, hash)
 
-        r = requests.get(download_page_link)
-        soup = BeautifulSoup(r.content, features='html.parser')
-        span = soup.find('span', {'style': 'background:#f9f9f9;border:1px dotted #bbb;padding:7px;'})
+            r = requests.get(download_page_link)
+            soup = BeautifulSoup(r.content, features='html.parser')
+            span = soup.find('span', {'style': 'background:#f9f9f9;border:1px dotted #bbb;padding:7px;'})
 
-    download_link = span.find('a')['href']
-    return download_link
+        if span is not None:
+            return span.find('a')['href']
+        
+    return None
